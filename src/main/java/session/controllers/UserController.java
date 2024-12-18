650177de60f8d5450313dd0553256bfa7e3fa7f7
TramLeManh <ITITIU21077@student.hcmiu.edu.vn>
2024-12-18 15:16:53 +0700

    update

--- src/main/java/session/controllers/UserController.java
+++ src/main/java/session/controllers/UserController.java
@@ -88,12 +88,12 @@ public class UserController {
             return "error";
         }
     }
-
-
     @GetMapping("/getUserBooking")
     public String getUserBooking(HttpSession session, @RequestParam(required = false) Integer status, Model model) {
+        Integer user_id = (Integer) session.getAttribute("user");
         UserInformation user = userInformationRepo.getUserInformation(8242);
         List<bookTableDTO> bookings = bookingService.getUserBooking(8242, status);
+
         model.addAttribute("currentStatus", status);
         model.addAttribute("bookingTable", bookings);
         model.addAttribute("user", user);
